fc3560fe6272023571261926ba85765e80c4c1e5
Charlotte Kam <>
2022-03-15 13:58:18 +0000

    added football function

--- bot.js
+++ bot.js
@@ -2,16 +2,22 @@ function answerQuery(query) {
   query = query.toLowerCase();
   if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1)
     return "I do not care too much about weather, I'm locked inside a data center.";
+    else if (football(query)) {
+      return "Oh I know that game, the game which people chase after a ball for 90 minutes!"
+    }
   return "Sorry Dave, I cannot do that."
 }
 
+function football(query) {
+  return query.indexOf("football") != -1 ;
+}
 
 function handleSayClick() {
   // We first get the 'message' that the user entered and 'conversation' element
   // which we are using for showing all the conversation history
   var message = document.getElementById('message').value.toLowerCase();
   var conversation = document.getElementById('conversation');
-  
+
   // Get answer for the given query and show the query & answer
   var answer = answerQuery(message)
   conversation.innerHTML += "<p><strong>You:</strong> " + message + "</p>";
